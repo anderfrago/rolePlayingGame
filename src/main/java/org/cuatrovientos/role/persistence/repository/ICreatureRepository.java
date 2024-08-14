1784e853e1e7c2391a6815ddd4d6c229d7e987de
Ander F.L <ander_frago@cuatrovientos.org>
2024-08-14 08:57:13 +0200

    1 version: mvc

--- src/main/java/org/cuatrovientos/role/persistence/repository/ICreatureRepository.java
+++
@@ -1,8 +0,0 @@
-package org.cuatrovientos.role.persistence.repository;
-
-import org.cuatrovientos.role.persistence.model.Creature;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ICreatureRepository extends CrudRepository<Creature, Long> {
-
-}
