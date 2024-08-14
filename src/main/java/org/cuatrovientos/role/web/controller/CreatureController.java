2eb053b6954410b06dfb82ad916e2f9651f952d0
Ander F.L <ander_frago@cuatrovientos.org>
2024-08-14 15:24:31 +0200

    rest-1-crud

--- src/main/java/org/cuatrovientos/role/web/controller/CreatureController.java
+++ src/main/java/org/cuatrovientos/role/web/controller/CreatureController.java
@@ -6,13 +6,13 @@ import java.util.List;
 import org.cuatrovientos.role.persistence.model.Creature;
 import org.cuatrovientos.role.service.ICreatureService;
 import org.cuatrovientos.role.web.dto.CreatureDto;
-import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,7 +48,7 @@ public class CreatureController {
     }
     
     @GetMapping("/delete/{id}")
-    public Creature deleteCreature(@ModelAttribute("Creature") CreatureDto creature, BindingResult bindingResult) {
+    public Creature deleteCreature(@RequestBody CreatureDto creature, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new Creature();
         }
@@ -57,7 +57,7 @@ public class CreatureController {
     }
     
     @PostMapping("/edit/{id}")
-    public Creature modifyCreature(@ModelAttribute("Creature") CreatureDto creature, BindingResult bindingResult) {
+    public Creature modifyCreature(@RequestBody CreatureDto creature, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new Creature();
         }
@@ -68,12 +68,12 @@ public class CreatureController {
 	
 
     @PostMapping
-    public Creature addCreature(@ModelAttribute("Creature") CreatureDto Creature, BindingResult bindingResult) {
+    public Creature addCreature(@RequestBody CreatureDto creature, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new Creature();
         }
 
-        Creature newCreature = creatureSercice.save(convertCreatureToEntity(Creature));
+        Creature newCreature = creatureSercice.save(convertCreatureToEntity(creature));
         return newCreature;
     }	
 	
